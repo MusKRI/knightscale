37b35c3bc2fc3c1d817daa37fb2adf6f13738fd4
Krishna <krishna13248@gmail.com>
2023-11-27 14:45:11 +0530

    v1

---
+++ components/Blog/blogData.tsx
@@ -0,0 +1,47 @@
+import { Blog } from "@/types/blog";
+
+const blogData: Blog[] = [
+  {
+    id: 1,
+    title: "SellSphere",
+    paragraph:
+      "Welcome to SellSphere, your dynamic e-commerce solution! Offering a comprehensive suite of services akin to Shopify, SellSphere is your go-to platform for seamless online selling. Empowering businesses with intuitive tools, robust functionalities, and unparalleled support, SellSphere fosters growth and success. Embrace a user-friendly interface, customizable features, and dedicated assistance—making your online selling journey effortless and lucrative",
+    image: "/images/blog/blog-01.jpg",
+    author: {
+      name: " ",
+      image: "",
+      designation: "",
+    },
+    tags: ["Business"],
+    publishDate: "",
+  },
+  {
+    id: 2,
+    title: "KnightScaler",
+    paragraph:
+      "Unlock the power of Knightscale UI for effortless front-end design. Plus, with Plus Membership, a click builds your backend instantly. Now with the help of our SellSpehere you can manage your store and inventory easily. Seamlessly expand your business with this all-in-one solution. Elevate your online presence, streamline operations, and reach new heights effortlessly. Your single destination for a potent, scalable, and hassle-free online journey!",
+    image: "/images/blog/blog-02.jpg",
+    author: {
+      name: " ",
+      image: "",
+      designation: "",
+    },
+    tags: ["Design"],
+    publishDate: "",
+  },
+  {
+    id: 3,
+    title: "Knight Scale UI",
+    paragraph:
+      "Introducing a revolutionary website: a platform that transforms static websites effortlessly using Knightscale UI Library. With Knightscale UI, you wield the power to customize components, tailoring your website precisely to your needs. Embrace a seamless journey from static to dynamic, crafting a website that truly reflects your vision and requirements. Elevate your online presence with ease and precision, all thanks to Knightscale UI.",
+    image: "/images/blog/blog-03.jpg",
+    author: {
+      name: "",
+      image: "",
+      designation: "",
+    },
+    tags: ["design"],
+    publishDate: " ",
+  },
+];
+export default blogData;
